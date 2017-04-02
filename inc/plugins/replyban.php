643036e9dff3608878fefc0948a713d16a9055d8
PaulBender <paulbender@live.com>
2017-04-02 16:07:47 -0400

    Use THIS_SCRIPT for template caching

--- inc/plugins/replyban.php
+++ inc/plugins/replyban.php
@@ -11,7 +11,7 @@ if(!defined("IN_MYBB"))
 }
 
 // Neat trick for caching our custom template(s)
-if(my_strpos($_SERVER['PHP_SELF'], 'showthread.php'))
+if(THIS_SCRIPT == 'showthread.php')
 {
 	global $templatelist;
 	if(isset($templatelist))
@@ -21,7 +21,7 @@ if(my_strpos($_SERVER['PHP_SELF'], 'showthread.php'))
 	$templatelist .= 'showthread_replybanlink';
 }
 
-if(my_strpos($_SERVER['PHP_SELF'], 'moderation.php'))
+if(THIS_SCRIPT == 'moderation.php')
 {
 	global $templatelist;
 	if(isset($templatelist))
