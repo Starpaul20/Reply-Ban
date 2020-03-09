d8cbff7644f13e4ba4e01a022ad5a5a83ba8cc1b
Paul Bender <paulbender@live.com>
2020-03-09 13:19:20 -0400

    Bump to version v1.4

--- inc/plugins/replyban.php
+++ inc/plugins/replyban.php
@@ -64,7 +64,7 @@ function replyban_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.3",
+		"version"			=> "1.4",
 		"codename"			=> "replyban",
 		"compatibility"		=> "18*"
 	);
